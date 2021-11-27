2a26991cde35b3870b2f151de268a05da7a5a2b3
Denis Carriere <carriere.denis@gmail.com>
2021-11-26 22:08:51 -0500

    add beebop example

--- plugins/plugins.cpp
+++ plugins/plugins.cpp
@@ -1,5 +1,5 @@
 #include "example.hpp"
-#include "memewarsreal.hpp"
+#include "robotbeepbop.hpp"
 
 namespace gems {
     pair<ATTRIBUTE_MAP, ATTRIBUTE_MAP> blend::mint_attributes( const name owner, const name collection_name, const int32_t template_id, const vector<uint64_t>& in_asset_ids )
@@ -14,8 +14,8 @@ namespace gems {
         switch ( collection_name.value ) {
             case "example"_n.value:
                 return example::mint_attributes(owner, collection_name, template_id, in_asset_ids, in_assets );
-            case "memewarsreal"_n.value:
-                return memewarsreal::mint_attributes(owner, collection_name, template_id, in_asset_ids, in_assets );
+            case "robotbeepbop"_n.value:
+                return robotbeepbop::mint_attributes(owner, collection_name, template_id, in_asset_ids, in_assets );
         }
         return { };
     }
