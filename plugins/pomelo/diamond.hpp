2acc07a9b24a052612bf89c54901e530d911400a
YaroShkvorets <shkvorets@gmail.com>
2021-11-29 21:01:40 -0500

    define maps

--- plugins/pomelo/diamond.hpp
+++ plugins/pomelo/diamond.hpp
@@ -3,7 +3,63 @@
 
 namespace pomelo {
 
-    string random_select( const vector<string> values )
+    const map<string, int> COLOR_SCORE = {
+        // colorless
+        { "D", 100 },
+        { "E", 100 },
+        { "F", 100 },
+        // near colorless
+        { "G", 80 },
+        { "H", 80 },
+        { "I", 80 },
+        { "J", 80 },
+        // faint yellow
+        { "K", 60 },
+        { "L", 60 },
+        { "M", 60 },
+        // very light yellow
+        { "N", 40 },
+        { "O", 40 },
+        { "P", 40 },
+        { "R", 40 },
+        // light yellow
+        { "S", 20 },
+        { "T", 20 },
+        { "U", 20 },
+        { "V", 20 },
+        { "W", 20 },
+        { "X", 20 },
+        { "Y", 20 },
+        { "Z", 20 }
+    };
+
+    const map<string, int> CLARITY_SCORE = {
+        { "FL", 100 },
+        { "IF", 100 },
+        { "VVS1", 80 },
+        { "VVS2", 80 },
+        { "VS1", 60 },
+        { "VS2", 60 },
+        { "SI1", 40 },
+        { "SI2", 40 },
+        { "I1", 20 },
+        { "I2", 20 },
+        { "I3", 20 }
+    };
+
+    int calculate_color( const string& color )
+    {
+        check( COLOR_SCORE.count(color), "pomelo::diamond::calculate_color: [color] invalid value");
+        return COLOR_SCORE.at(color);
+    }
+
+    int calculate_clarity( const string& clarity )
+    {
+        check( CLARITY_SCORE.count(clarity), "pomelo::diamond::calculate_clarity: [clarity] invalid value");
+        return CLARITY_SCORE.at(clarity);
+    }
+
+    string random_select( const vector<string>& values )
     {
         check( values.size(), "pomelo::diamond::random_select: [values] must contain at least one item");
         return values.at(gems::random::generate(1, 0, values.size() -1 )[0]);
@@ -27,216 +83,143 @@ namespace pomelo {
         return random_select({"I1", "I2", "I3"});
     }
 
-    int calculate_color( const name color )
-    {
-        switch ( color.value ) {
-            // colorless
-            case "d"_n.value:
-            case "e"_n.value:
-            case "f"_n.value:
-                return 100;
-            // near colorless
-            case "g"_n.value:
-            case "h"_n.value:
-            case "i"_n.value:
-            case "j"_n.value:
-                return 80;
-            // faint yellow
-            case "k"_n.value:
-            case "l"_n.value:
-            case "m"_n.value:
-                return 60;
-            // very light yellow
-            case "n"_n.value:
-            case "o"_n.value:
-            case "p"_n.value:
-            case "q"_n.value:
-            case "r"_n.value:
-                return 40;
-            // light yellow
-            case "s"_n.value:
-            case "t"_n.value:
-            case "u"_n.value:
-            case "v"_n.value:
-            case "w"_n.value:
-            case "x"_n.value:
-            case "y"_n.value:
-            case "z"_n.value:
-                return 20;
-        }
-        check( false, "pomelo::diamond::calculate_color: [color] invalid value");
-        return 0;
-    }
-    int calculate_clarity( const name clarity )
-    {
-        switch ( clarity.value ) {
-            // Internally Flawless
-            case "fl"_n.value:
-            case "if"_n.value:
-                return 100;
-
-            // Very Very Slight Inclusions
-            case "vvs1"_n.value:
-            case "vvs2"_n.value:
-                return 80;
-
-            // Very Slight Inclusions
-            case "vs1"_n.value:
-            case "vs2"_n.value:
-                return 60;
-
-            // Slight Inclusions
-            case "si1"_n.value:
-            case "si2"_n.value:
-                return 40;
-
-            // Imperfect
-            case "i1"_n.value:
-            case "i2"_n.value:
-            case "i3"_n.value:
-                return 20;
-        }
-        check( false, "pomelo::diamond::calculate_clarity: [clarity] invalid value");
-        return 0;
-    }
-
     string select_img( const ATTRIBUTE_MAP& immutable_data )
     {
-        const name shape = atomic::attribute_to_name( immutable_data, "shape" );
-        const int color_score = calculate_color( atomic::attribute_to_name( immutable_data, "color" ) );
-        const int clarity_score = calculate_clarity( atomic::attribute_to_name( immutable_data, "clarity" ) );
+        const string shape = atomic::attribute_to_string( immutable_data, "shape" );
+        const int color_score = calculate_color( atomic::attribute_to_string( immutable_data, "color" ) );
+        const int clarity_score = calculate_clarity( atomic::attribute_to_string( immutable_data, "clarity" ) );
 
-        switch ( shape.value ) {
-            case "round"_n.value:
-                switch ( color_score ) {
-                    case 100:
-                        switch ( clarity_score ) {
-                            case 100: return "IPFS";
-                            case 80: return "IPFS";
-                            case 60: return "IPFS";
-                            case 40: return "IPFS";
-                            case 20: return "IPFS";
-                        }
-                    case 80:
-                        switch ( clarity_score ) {
-                            case 100: return "IPFS";
-                            case 80: return "IPFS";
-                            case 60: return "IPFS";
-                            case 40: return "IPFS";
-                            case 20: return "IPFS";
-                        }
-                    case 60:
-                        switch ( clarity_score ) {
-                            case 100: return "IPFS";
-                            case 80: return "IPFS";
-                            case 60: return "IPFS";
-                            case 40: return "IPFS";
-                            case 20: return "IPFS";
-                        }
-                    case 40:
-                        switch ( clarity_score ) {
-                            case 100: return "IPFS";
-                            case 80: return "IPFS";
-                            case 60: return "IPFS";
-                            case 40: return "IPFS";
-                            case 20: return "IPFS";
-                        }
-                    case 20:
-                        switch ( clarity_score ) {
-                            case 100: return "IPFS";
-                            case 80: return "IPFS";
-                            case 60: return "IPFS";
-                            case 40: return "IPFS";
-                            case 20: return "IPFS";
-                        }
-                }
-            case "square"_n.value:
-                switch ( color_score ) {
-                    case 100:
-                        switch ( clarity_score ) {
-                            case 100: return "IPFS";
-                            case 80: return "IPFS";
-                            case 60: return "IPFS";
-                            case 40: return "IPFS";
-                            case 20: return "IPFS";
-                        }
-                    case 80:
-                        switch ( clarity_score ) {
-                            case 100: return "IPFS";
-                            case 80: return "IPFS";
-                            case 60: return "IPFS";
-                            case 40: return "IPFS";
-                            case 20: return "IPFS";
-                        }
-                    case 60:
-                        switch ( clarity_score ) {
-                            case 100: return "IPFS";
-                            case 80: return "IPFS";
-                            case 60: return "IPFS";
-                            case 40: return "IPFS";
-                            case 20: return "IPFS";
-                        }
-                    case 40:
-                        switch ( clarity_score ) {
-                            case 100: return "IPFS";
-                            case 80: return "IPFS";
-                            case 60: return "IPFS";
-                            case 40: return "IPFS";
-                            case 20: return "IPFS";
-                        }
-                    case 20:
-                        switch ( clarity_score ) {
-                            case 100: return "IPFS";
-                            case 80: return "IPFS";
-                            case 60: return "IPFS";
-                            case 40: return "IPFS";
-                            case 20: return "IPFS";
-                        }
-                }
-
-            case "triangle"_n.value:
-                switch ( color_score ) {
-                    case 100:
-                        switch ( clarity_score ) {
-                            case 100: return "IPFS";
-                            case 80: return "IPFS";
-                            case 60: return "IPFS";
-                            case 40: return "IPFS";
-                            case 20: return "IPFS";
-                        }
-                    case 80:
-                        switch ( clarity_score ) {
-                            case 100: return "IPFS";
-                            case 80: return "IPFS";
-                            case 60: return "IPFS";
-                            case 40: return "IPFS";
-                            case 20: return "IPFS";
-                        }
-                    case 60:
-                        switch ( clarity_score ) {
-                            case 100: return "IPFS";
-                            case 80: return "IPFS";
-                            case 60: return "IPFS";
-                            case 40: return "IPFS";
-                            case 20: return "IPFS";
-                        }
-                    case 40:
-                        switch ( clarity_score ) {
-                            case 100: return "IPFS";
-                            case 80: return "IPFS";
-                            case 60: return "IPFS";
-                            case 40: return "IPFS";
-                            case 20: return "IPFS";
-                        }
-                    case 20:
-                        switch ( clarity_score ) {
-                            case 100: return "IPFS";
-                            case 80: return "IPFS";
-                            case 60: return "IPFS";
-                            case 40: return "IPFS";
-                            case 20: return "IPFS";
-                        }
-                }
+        if ( shape == "Round") {
+            switch ( color_score ) {
+                case 100:
+                    switch ( clarity_score ) {
+                        case 100: return "IPFS";
+                        case 80: return "IPFS";
+                        case 60: return "IPFS";
+                        case 40: return "IPFS";
+                        case 20: return "IPFS";
+                    }
+                case 80:
+                    switch ( clarity_score ) {
+                        case 100: return "IPFS";
+                        case 80: return "IPFS";
+                        case 60: return "IPFS";
+                        case 40: return "IPFS";
+                        case 20: return "IPFS";
+                    }
+                case 60:
+                    switch ( clarity_score ) {
+                        case 100: return "IPFS";
+                        case 80: return "IPFS";
+                        case 60: return "IPFS";
+                        case 40: return "IPFS";
+                        case 20: return "IPFS";
+                    }
+                case 40:
+                    switch ( clarity_score ) {
+                        case 100: return "IPFS";
+                        case 80: return "IPFS";
+                        case 60: return "IPFS";
+                        case 40: return "IPFS";
+                        case 20: return "IPFS";
+                    }
+                case 20:
+                    switch ( clarity_score ) {
+                        case 100: return "IPFS";
+                        case 80: return "IPFS";
+                        case 60: return "IPFS";
+                        case 40: return "IPFS";
+                        case 20: return "IPFS";
+                    }
+            }
+        }
+        if ( shape == "Square" ) {
+            switch ( color_score ) {
+                case 100:
+                    switch ( clarity_score ) {
+                        case 100: return "IPFS";
+                        case 80: return "IPFS";
+                        case 60: return "IPFS";
+                        case 40: return "IPFS";
+                        case 20: return "IPFS";
+                    }
+                case 80:
+                    switch ( clarity_score ) {
+                        case 100: return "IPFS";
+                        case 80: return "IPFS";
+                        case 60: return "IPFS";
+                        case 40: return "IPFS";
+                        case 20: return "IPFS";
+                    }
+                case 60:
+                    switch ( clarity_score ) {
+                        case 100: return "IPFS";
+                        case 80: return "IPFS";
+                        case 60: return "IPFS";
+                        case 40: return "IPFS";
+                        case 20: return "IPFS";
+                    }
+                case 40:
+                    switch ( clarity_score ) {
+                        case 100: return "IPFS";
+                        case 80: return "IPFS";
+                        case 60: return "IPFS";
+                        case 40: return "IPFS";
+                        case 20: return "IPFS";
+                    }
+                case 20:
+                    switch ( clarity_score ) {
+                        case 100: return "IPFS";
+                        case 80: return "IPFS";
+                        case 60: return "IPFS";
+                        case 40: return "IPFS";
+                        case 20: return "IPFS";
+                    }
+            }
+        }
+        if ( shape == "Triangle" ) {
+            switch ( color_score ) {
+                case 100:
+                    switch ( clarity_score ) {
+                        case 100: return "IPFS";
+                        case 80: return "IPFS";
+                        case 60: return "IPFS";
+                        case 40: return "IPFS";
+                        case 20: return "IPFS";
+                    }
+                case 80:
+                    switch ( clarity_score ) {
+                        case 100: return "IPFS";
+                        case 80: return "IPFS";
+                        case 60: return "IPFS";
+                        case 40: return "IPFS";
+                        case 20: return "IPFS";
+                    }
+                case 60:
+                    switch ( clarity_score ) {
+                        case 100: return "IPFS";
+                        case 80: return "IPFS";
+                        case 60: return "IPFS";
+                        case 40: return "IPFS";
+                        case 20: return "IPFS";
+                    }
+                case 40:
+                    switch ( clarity_score ) {
+                        case 100: return "IPFS";
+                        case 80: return "IPFS";
+                        case 60: return "IPFS";
+                        case 40: return "IPFS";
+                        case 20: return "IPFS";
+                    }
+                case 20:
+                    switch ( clarity_score ) {
+                        case 100: return "IPFS";
+                        case 80: return "IPFS";
+                        case 60: return "IPFS";
+                        case 40: return "IPFS";
+                        case 20: return "IPFS";
+                    }
+            }
         }
         check( false, "pomelo::diamond::select_img: invalid value");
         return "";
@@ -250,11 +233,11 @@ namespace pomelo {
         int clarity_score = 0;
 
         // add shapes & sum scores
-        for ( const auto asset : in_assets ) {
+        for ( const auto& asset : in_assets ) {
             const ATTRIBUTE_MAP immutable = atomic::get_template_immutable( asset );
             const string shape = atomic::attribute_to_string( immutable, "shape" );
-            const name color = atomic::attribute_to_name( immutable, "color" );
-            const name clarity = atomic::attribute_to_name( immutable, "clarity" );
+            const string color = atomic::attribute_to_string( immutable, "color" );
+            const string clarity = atomic::attribute_to_string( immutable, "clarity" );
 
             // accumulate all shapes (higher chance of selecting the more as input)
             shapes.push_back( shape );
