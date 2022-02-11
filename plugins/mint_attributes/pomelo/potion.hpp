80fe6951f34ebc76a4323a3374155387ca1b1466
Denis Carriere <carriere.denis@gmail.com>
2022-02-11 09:25:31 -0500

    update immutable

--- plugins/mint_attributes/pomelo/potion.hpp
+++ plugins/mint_attributes/pomelo/potion.hpp
@@ -6,12 +6,16 @@ namespace pomelo {
 namespace potion {
     pair<ATTRIBUTE_MAP, ATTRIBUTE_MAP> mint_attributes( const name owner, const name collection_name, const int32_t template_id, const vector<uint64_t>& in_asset_ids, const vector<atomicassets::assets_s>& in_assets )
     {
+        // immutable
+        ATTRIBUTE_MAP immutable_data = {};
+        immutable_data["nonce"] = gems::random::generate(1)[0]; // int64
+        immutable_data["timestamp"] = current_time_point().sec_since_epoch(); // uint32
+
+        // mutable
         ATTRIBUTE_MAP mutable_data = {};
         mutable_data["quantity"] = 10; // int16
-        mutable_data["nonce"] = gems::random::generate(1)[0]; // int64
-        mutable_data["timestamp"] = current_time_point().sec_since_epoch(); // uint32
 
-        return { {}, mutable_data };
+        return { immutable_data, mutable_data };
     }
 } // namespace diamond
 } // namespace pomelo
